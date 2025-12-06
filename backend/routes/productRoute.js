2a0422a51539c3f0bc9d63e9971d7107e3600bd1
RaviRash2k <150897047+RaviRash2k@users.noreply.github.com>
2025-12-06 23:00:55 +0530

    add middleware to product router

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import { addProduct, deleteProduct, getAllProducts, getOneProduct, updateProduct } from '../controllers/productController';
+import verifyToken from '../middlewares/auth';
 
 const productRoute = express.Router();
 
-productRoute.post('/add', addProduct)
-productRoute.get('/add', getOneProduct)
-productRoute.get('/add', getAllProducts)
-productRoute.post('/add', updateProduct)
-productRoute.post('/add', deleteProduct)
+productRoute.post('/add', verifyToken, addProduct)
+productRoute.get('/add', verifyToken, getOneProduct)
+productRoute.get('/add', verifyToken, getAllProducts)
+productRoute.post('/add', verifyToken, updateProduct)
+productRoute.post('/add', verifyToken, deleteProduct)
 
 
 export default productRoute;
